499bc7c9b7c8eaed7730e024fbfaf85c0a61096c
Gyömörey Domonkos <69725469+domonkosgyomorey@users.noreply.github.com>
2025-05-24 19:03:41 +0200

    blockchain update

--- simulation/blockchain.py
+++ simulation/blockchain.py
@@ -1,40 +1,40 @@
 import hashlib
-from queue import Queue
+from collections import deque
 import time
-from simulation.household import Household
-from typing import Optional, List
-
+from typing import Literal
 class Offer:
-    def __init__(self, seller_id: str, buyer_id: str, amount: float, timestamp: Optional[float] = None):
-        self.seller_id = seller_id
-        self.buyer_id = buyer_id
+    def __init__(self, seller_id: str, buyer_id: str, amount: float):
+        self.seller_address = seller_id
+        self.buyer_address = buyer_id
         self.amount = amount
-        self.timestamp = timestamp or time.time()
 
     def to_string(self) -> str:
-        offer=f"{self.seller_id};{self.buyer_id};{self.amount};{self.timestamp}"
-        return offer
+        return f"{self.seller_address};{self.buyer_address};{self.amount};"
+
+    @staticmethod
+    def from_dict(data: dict[Literal["seller_id", "buyer_id", "amount", "iter_index"], str | float | int]) -> None:
+       Offer(seller_id=str(data["seller_id"]), buyer_id=str(data["buyer_id"]), amount=float(data["amount"])) 
     
 class HouseholdData:
-    def __init__(self,  id: str, production: Queue, consumption:Queue, battery: float):
+    def __init__(self,  id: str, forecast_history_size):
         self.id:str = id
-        self.production = production
-        self.consumption = consumption
-        self.stored_kwh: float = battery
+        self.production: deque[float] = deque(maxlen=forecast_history_size) 
+        self.consumption: deque[float] = deque(maxlen=forecast_history_size)
+        self.token: float = 0
 
     def to_dict(self) -> dict:
         return {
             "id": self.id,
             "production": self.production,
             "consumption": self.consumption,
-            "stored_kwh": self.stored_kwh
+            "token": self.token
         }
 
 class Block:
     def __init__(self, index: int, timestamp: float, data: str, previous_hash: str):
         self.index = index
         self.timestamp = timestamp
-        self.data: str = data #deal info
+        self.data: str = data
         self.previous_hash = previous_hash
         self.hash = self.calculate_hash()
 
@@ -45,11 +45,11 @@ class Block:
 class Blockchain:
     def __init__(self, forecast_history_size: int, forecast_prediction_size: int):
         self.chain: list[Block] = [self.create_genesis_block()]
-        self.household_data_list: list[HouseholdData] = []
-        self.pending_offers: list[list[Offer]] = []
-        #self.done_deals: list[Offer] = []
+        self.households: dict[str, HouseholdData] = {}
+        self.offers: list[list[Offer]] = []
+        
         self.forecast_history_size = forecast_history_size
-        self.forecast_history_prediction = forecast_prediction_size
+        self.forecast_prediction_size = forecast_prediction_size
 
     def create_genesis_block(self) -> Block:
         return Block(0, time.time(), "Genesis Block", "0")
@@ -62,19 +62,26 @@ class Blockchain:
         new_block = Block(len(self.chain), time.time(), data, previous_block.hash)
         self.chain.append(new_block)
 
-    def upsert_household_data(self, id: str, production: float, consumption: float, battery) -> None:
-
-        self.household_data_list.append(HouseholdData(id, production, consumption, battery))
+    def add_household_data(self, id: str, production: float, consumption: float, stored_kwh: float) -> None:
+        if self.households.get(id) is None:
+            self.households[id] = HouseholdData(id=id,forecast_history_size=self.forecast_prediction_size)
+        
+        self.households[id].production.append(production)
+        self.households[id].consumption.append(consumption)
+        self.households[id].token = stored_kwh
 
     def get_households_data(self) -> list[dict]:
-        return [household.to_dict() for household in self.household_data_list]
+        return [household.to_dict() for _, household in self.households.items()]
+
 
     def update_households(self) -> None:
         # Itt majd implementáld az update logikát
         pass
 
-    def add_offer(self, offers: list[Offer]) -> None:
-        self.pending_offers.append(offers)
+    def add_offer(self, offers: list[dict]) -> None:
+        self.offers = [[] for _ in range(self.forecast_prediction_size)]
+        for offer in offers:
+            self.offers[offer["iter_index"]].append(Offer(buyer_id=offer["buyer_id"], seller_id=offer["seller_id"], amount=offer["amount"]))
 
     def trade_on_every_15(self) -> None:
         # A blokk adat egy string, az első pending_offers lista összes ajánlatának string összefűzve, soronként:
